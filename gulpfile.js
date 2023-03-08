9f88823e75fa73fa77b32e4b7bb67b8a401c3628
Nicholas Gimbel <56981761+ngimb64@users.noreply.github.com>
2023-03-08 16:12:59 -0500

    3/8/23 - gulp watcher task added for autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 
-
 gulp.task('check', function() {
     return new Promise(function(resolve, reject) {
         console.log('\nGulp Initiated!!!\n');
@@ -9,11 +8,15 @@ gulp.task('check', function() {
     });
 });
 
-gulp.task('prefix', function () {
-    return gulp.src('assets/pre_css/*.css')
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 99 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('assets/css'))}
-);
+gulp.task('watch', function () {
+    gulp.watch('assets/pre_css/*.css', {events: ['add', 'change']}, function () {
+        return gulp.src('assets/pre_css/*.css')
+            .pipe(autoprefixer({
+                overrideBrowserslist: ['last 99 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest('assets/css'))
+    });
+});
+
+//gulp.task('default', gulp.series('check', 'watch'));
